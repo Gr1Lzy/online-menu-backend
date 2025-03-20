736cd563aa01e13dadbe1353cd1c2472fc07cc52
Andrii Kolomoiets <kolomoets02@gmail.com>
2025-03-20 13:19:00 +0200

    [main] FEATURE | Created project

---
+++ src/main/java/com/github/menu/OnlineMenuBackendApplication.java
@@ -0,0 +1,12 @@
+package com.github.menu;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OnlineMenuBackendApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(OnlineMenuBackendApplication.class, args);
+  }
+}
