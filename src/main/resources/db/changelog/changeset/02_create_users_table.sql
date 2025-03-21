7cc2d43d7d3ae12807df4e3ad068470f0bf0875b
Andrii Kolomoiets <kolomoets02@gmail.com>
2025-03-21 12:24:42 +0200

    [main] FIX | Updated table.sql script

--- src/main/resources/db/changelog/changeset/02_create_users_table.sql
+++ src/main/resources/db/changelog/changeset/02_create_users_table.sql
@@ -5,7 +5,7 @@ CREATE TABLE IF NOT EXISTS public.users
 (
     id         BIGINT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
     username   VARCHAR(50) NOT NULL UNIQUE,
-    password   VARCHAR(50) NOT NULL,
+    password   VARCHAR(200) NOT NULL,
     email      VARCHAR(50) NOT NULL UNIQUE,
     role_id    BIGINT      NOT NULL REFERENCES public.roles (id),
     created_at TIMESTAMP   NOT NULL DEFAULT CURRENT_TIMESTAMP,
